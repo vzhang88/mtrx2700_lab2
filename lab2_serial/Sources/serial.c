10b45d6ad18ee36ef97d223f1ff13c0ae31bc0e1
vzhang88 <47177261+vzhang88@users.noreply.github.com>
2021-04-29 03:50:34 +1000

    Update serial.c

--- lab2_serial/Sources/serial.c
+++ lab2_serial/Sources/serial.c
@@ -5,28 +5,8 @@
 
 #include "derivative.h"
 
-char array[200];
-char* ptr;
-char cx;
-char* dx;
-int send;  
-struct serialPort sc0;
- 
-
-void main(void) {
-  
-
-  Init_SCI0();
-
-	EnableInterrupts;
-	
-  for(;;) {
-    _FEED_COP(); /* feeds the dog */
-  } /* loop forever */
-  /* please make sure that you never leave main */
-}
-
-
+/******************************* KEY VARIABLES *******************************/
+struct port sc0;
 /******************************* SERIAL PORT SETUP **************************/
 /* Set-up the Serial Port SCI0 to use interrupts */   
 void Init_SCI0 (void) {
@@ -36,6 +16,9 @@ void Init_SCI0 (void) {
   SCI0CR1 = 0;          // Clear all options for SCI Control Register 1 
   SCI0CR2 = 0x2C;       // Enable the transmitter (TE), receiver (RE) and receive 
                         // interrupt enable (RIE) bits only 
+  
+  sc0.ptr = sc0.array;         // pointer points to start of array
+  sc0.dx =sc0.array; 
 }
 
 /******************************* HELPER FUNCTIONS ****************************/
@@ -62,33 +45,33 @@ __interrupt void SCI0_ISR(void) {
   if (SCI0SR1 & 0x20) {
  
      
-     cx = SCI0DRL;        // Read in the received byte in the data register SCI0DRL 
+     sc0.cx = SCI0DRL;        // Read in the received byte in the data register SCI0DRL 
      
-     if (cx != 0x0D) {    // Check if the received byte is not a carriage return character
+     if (sc0.cx != 0x0D) {    // Check if the received byte is not a carriage return character
           
-       *ptr = SCI0DRL;    // Store the received byte in an entry of a container array specified 
+       *(sc0.ptr) = SCI0DRL;    // Store the received byte in an entry of a container array specified 
                           // by the pointer  
        
-       putcSCI0(*ptr);    // Print the received byte to the terminal so we have an indication
+       putcSCI0(*(sc0.ptr));    // Print the received byte to the terminal so we have an indication
                           // of what we are typing 
        
-       if (cx == 0x08) {  // Check if the received byte is a backspace character 
+       if (sc0.cx == 0x08) {  // Check if the received byte is a backspace character 
             
-         ptr-- ;//move back string pointer
+         (sc0.ptr)-- ;//move back string pointer
          putcSCI0(0x20);   // Output a space
          putcSCI0(0x08);   // Output a backspace
           
        } else { 
            
-         ptr++;           // Increment the pointer by one to point to the next entry in the array 
+         (sc0.ptr)++;           // Increment the pointer by one to point to the next entry in the array 
         
        }  
       
      } else {             // The received byte is a carriage return character 
        
-       putcSCI0(cx);      // Print the carriage return character to terminal 
-       *ptr = 0;          // Add a null terminator to the end of the sequence of characters 
-       send = 1;          // Toggle the value of the boolean 'send' to indicate that we have sent a string across;             
+       putcSCI0(sc0.cx);      // Print the carriage return character to terminal 
+       *(sc0.ptr) = 0;          // Add a null terminator to the end of the sequence of characters 
+       sc0.send = 1;          // Toggle the value of the boolean 'send' to indicate that we have sent a string across;             
        SCI0CR2 = 0xAC;    // Set the   BIG CHANGE HERE
                           
      }       
@@ -99,19 +82,19 @@ __interrupt void SCI0_ISR(void) {
   /* Check to see if the transmit data register empty flag (TDRE) is set, in which case the SCI data
   register transfers the currently stored byte to the transmit shift register */ 
   
-  while ((SCI0SR1 & 0x80) && (send == 1)) {
+  while ((SCI0SR1 & 0x80) && (sc0.send == 1)) {
   
-    SCI0DRL = *dx;       // Print the current character in the array 
+    SCI0DRL = *(sc0.dx);       // Print the current character in the array 
     
-    if (dx == ptr) {     // Check if the current character in the array points to the same character
+    if ((sc0.dx) == (sc0.ptr)) {     // Check if the current character in the array points to the same character
                          // that was last entered into the data register
-     send = 0;           // Toggle the boolean 'send' to 0 to indicate that we have finished sending
+     sc0.send = 0;           // Toggle the boolean 'send' to 0 to indicate that we have finished sending
      SCI0CR2 = 0x2C;     // Enable the transmitter (TE), receiver (RE) and receive interrupt enable (RIE) bits 
      SCI0DRL = 0x0D;     // Print a carriage return character 
      break;              // Break out of the loop to indicate that we have finished transmitting
        
     }  
-    dx++;                // Increment the pointer to inspect the next element in the array to be transmitted 
+    (sc0.dx)++;                // Increment the pointer to inspect the next element in the array to be transmitted 
   }
 }
 
