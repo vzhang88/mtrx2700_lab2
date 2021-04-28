8dae1603eb2e95059805a135cac9c4dd653ea980
vzhang88 <47177261+vzhang88@users.noreply.github.com>
2021-04-29 09:29:40 +1000

    Update random.c

--- random.c
+++ random.c
@@ -1,15 +1,13 @@
-int printSerial(int** arr){
-    char str[100];
-    char types[6][12] = {"Add", "Multiply", "Divide", "Squareroot", "Sin", "Cos"};
-
-    /* Make an array of strings */
-    sprintf(str, "%11s %11s %11s %11s %11s %11s %11s", " ", "Add", "Multiply", "Divide", "Squareroot", "Sin", "Cos");
-    
-    /* Send this string across serial */
-    Init_out_SCI0(str);
-    
-    for(int i=0; i < 4; i++){
-        sprintf(str, "%11s %11d %11d %11d %11d %11d %11d", types[i], arr[i][0], arr[i][1], arr[i][2], arr[i][3], arr[i][4], arr[i][5]);
-        Init_out_SCI0(str);
-    }
-}
+/******************************* SERIAL PORT OUTPUT SETUP *********************/
+/* Set-up the Serial Port SCI0 to use interrupts */  
+void Init_out_SCI0 (char* line) {
+ 
+  SCI0BDH = 0x00;       // Configure the Baud Rate by writing to the Baud Rate register
+  SCI0BDL = 0x9C;      
+  SCI0CR1 = 0;          // Clear all options for SCI Control Register 1
+  SCI0CR2 = 0xAC;       // Enable the transmitter (TE), receiver (RE) and receive interrupt enable (RIE) bits only
+ 
+  sc0.ptr = line-1;      // Last inputted byte points to end of array
+  sc0.dx = line;           // Transmitting byte points to start of array
+  sc0.send = 1;
+}  
