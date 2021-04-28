5a2e074676d97f6816326b28dd1bf276543e4afa
vzhang88 <47177261+vzhang88@users.noreply.github.com>
2021-04-29 03:55:09 +1000

    Update serial.h

--- lab2_serial/Sources/serial.h
+++ lab2_serial/Sources/serial.h
@@ -2,28 +2,19 @@
 #define SERIAL_HEADER
 
 
-//function to initialise serial SCI0
-void Init_SCI0 (void);
-
-
-void putcSCI0 (char cx);
-
-void putsSCI0 (char *dx);
-
-
-
-
-void delay (int x);
+// Function to initialise serial SCI0
+void Init_SCI0 ();
  
+// Interrupt Service Routine for SCI0 
 __interrupt void SCI0_ISR(void);
 
-
-
-/*
-char getch (void);
-
-int getsSCI0 (char *ptr);
-*/
+struct port{
+   char array[200];      // Container array that contains the input for the Serial Port 
+   char* ptr;            // A pointer representing the last entered byte of input
+   char cx;              // A temporary variable storing a character 
+   char* dx;             // A pointer representing the current transmitted byte 
+   int send;             // A boolean representing whether we have finished transmitting (0) or not (1) 
+};
 
 
 #endif 
